faeccb5f917785af4d870ad2f4f87351b16bc969
zhijian <zhijian_233@qq.com>
2025-10-12 03:36:50 +0800

    删除jm.py中的无用代码

--- src/jm.py
+++ src/jm.py
@@ -12,22 +12,4 @@ def jmcomic_create_option_by_file() -> jmcomic.JmOption:
     jmbot_path = pathlib.Path(__file__).parent.parent.absolute()
     print(jmbot_path)
     os.environ['JMBOT_PATH'] = f'{jmbot_path}'
-    return jmcomic.create_option_by_file(f'{jmbot_path}/options/jmcomic_option.yml')    
-
-def jmdownloader_callback_factory(
-    func: Callable,
-    *fixed_args: Any,
-    **fixed_kwargs: Any
-) -> Callable[[Any, Any], Any]:
-    """
-    函数工厂，生成只接收前两个参数的回调函数
-    
-    :param func: 原始函数，需至少接收两个位置参数
-    :param fixed_args: 需要固定的位置参数
-    :param fixed_kwargs: 需要固定的关键字参数
-    :return: 接收两个参数的回调函数
-    """
-    @functools.wraps(func)
-    def wrapped_callback(album: jmcomic.JmAlbumDetail, downloader: jmcomic.JmDownloader) -> Any:
-        return func(album, downloader, *fixed_args, **fixed_kwargs)
-    return wrapped_callback
+    return jmcomic.create_option_by_file(f'{jmbot_path}/options/jmcomic_option.yml')
